3420d32c9f808c2543517affdf6ca5a8517b20e4
Hammas Rashid <125029180+hammas-coding@users.noreply.github.com>
2024-03-08 15:32:48 +0500

    Update GradientDescent-011.cpp

--- Lab5-GradientDescent/GradientDescent-011.cpp
+++ Lab5-GradientDescent/GradientDescent-011.cpp
@@ -1,122 +1,116 @@
-#include <iostream>
-#include <stdlib.h>
-#include<conio.h>
-using namespace std;
+#include <iostream> // Include input/output stream library
+#include <stdlib.h> // Include standard library for functions like exit()
+#include<conio.h> // Include console input/output library (specific to some environments)
+using namespace std; // Using the standard namespace
+
 /*
+Grammar:
 E->TE'
 E'->+TE'|-TE'|null
 T-> FT'
 T'->*FT'|/FT'|null
 F-> id|num|(E)
 */
-int count = 0;
+
+int count = 0; // Global variable to keep track of current position in the input string
+
+// Function prototypes
 void E();
 void Ed();
 void T();
 void Td();
 void F();
 
-string expr;
+string expr; // Global variable to store the input expression
 
 int main() {
-  cin >> expr;
-  int l = expr.length();
-  expr += "$";
-  E();
-  if (l == count)
-  {
-  
-    cout << "Accepted" << endl;
-    getchar();
-  }else
-  
-    cout << "Rejected" << endl;
-getchar();
+  cin >> expr; // Take input expression from the user
+  int l = expr.length(); // Get length of input expression
+  expr += "$"; // Append '$' to the end of the expression to indicate end of input
+  E(); // Call the starting symbol of the grammar
+  if (l == count) { // If entire expression is parsed
+    cout << "Accepted" << endl; // Print Accepted
+    getchar(); // Wait for user input
+  } else
+    cout << "Rejected" << endl; // Print Rejected
+  getchar(); // Wait for user input
 }
 
 void E() {
-  cout << "E->TE'" << endl;
-  getchar();
-  T();
-  Ed();
+  cout << "E->TE'" << endl; // Print production rule being applied
+  getchar(); // Wait for user input
+  T(); // Call non-terminal T
+  Ed(); // Call non-terminal E'
 }
 
 void Ed() {
-  if (expr[count] == '+') {
-    count++;
-    cout << "E'->+TE'" << endl;
-   getchar();
-    T();
-    Ed();
-  }
-
-  else if (expr[count] == '-') {
-    count++;
-    cout << "E'->-TE'" << endl;
-    getchar();
-	T();
-    Ed();
-  }
-
-  else {
-    cout << "E'->null" << endl;
-  getchar();
+  if (expr[count] == '+') { // If next symbol is '+'
+    count++; // Move to the next symbol
+    cout << "E'->+TE'" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+    T(); // Call non-terminal T
+    Ed(); // Call non-terminal E'
+  } else if (expr[count] == '-') { // If next symbol is '-'
+    count++; // Move to the next symbol
+    cout << "E'->-TE'" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+    T(); // Call non-terminal T
+    Ed(); // Call non-terminal E'
+  } else {
+    cout << "E'->null" << endl; // If next symbol is not '+' or '-', print null production
+    getchar(); // Wait for user input
   }
 }
 
 void T() {
-  cout << "T->FT'" << endl;
-  getchar();
-  F();
-  Td();
+  cout << "T->FT'" << endl; // Print production rule being applied
+  getchar(); // Wait for user input
+  F(); // Call non-terminal F
+  Td(); // Call non-terminal T'
 }
 
 void Td() {
-  if (expr[count] == '*') {
-    count++;
-    cout << "T'->*FT'" << endl;
-    getchar();
-	F();
-    Td();
-  }
-
-  else if (expr[count] == '/') {
-    count++;
-    cout << "T'->/FT'" << endl;
-    getchar();
-	F();
-    Td();
-  }
-
-  else {
-    cout << "T'->null" << endl;
-  getchar();
+  if (expr[count] == '*') { // If next symbol is '*'
+    count++; // Move to the next symbol
+    cout << "T'->*FT'" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+    F(); // Call non-terminal F
+    Td(); // Call non-terminal T'
+  } else if (expr[count] == '/') { // If next symbol is '/'
+    count++; // Move to the next symbol
+    cout << "T'->/FT'" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+    F(); // Call non-terminal F
+    Td(); // Call non-terminal T'
+  } else {
+    cout << "T'->null" << endl; // If next symbol is neither '*' nor '/', print null production
+    getchar(); // Wait for user input
   }
 }
 
 void F() {
-  if (isalpha(expr[count])) {
-    count++;
-    cout << "F->id" << endl;
-  getchar();
-  } else if (isdigit(expr[count])) {
-    count++;
-    cout << "F->digit" << endl;
-  getchar();
-  } else if (expr[count] == '(') {
-    count++;
-    cout << "F->(E)" << endl;
-    getchar();
-	E();
-    if (expr[count] != ')') {
-      cout << "Rejected" << endl;
-      getchar();
-	  exit(0);
+  if (isalpha(expr[count])) { // If next symbol is alphabetic
+    count++; // Move to the next symbol
+    cout << "F->id" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+  } else if (isdigit(expr[count])) { // If next symbol is a digit
+    count++; // Move to the next symbol
+    cout << "F->digit" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+  } else if (expr[count] == '(') { // If next symbol is '('
+    count++; // Move to the next symbol
+    cout << "F->(E)" << endl; // Print production rule being applied
+    getchar(); // Wait for user input
+    E(); // Call non-terminal E
+    if (expr[count] != ')') { // If ')' does not follow '('
+      cout << "Rejected" << endl; // Print Rejected
+      getchar(); // Wait for user input
+      exit(0); // Exit the program
     }
-    count++;
+    count++; // Move to the next symbol
   } else {
-    cout << "Rejected" << endl;
-    getchar();
-	exit(0);
+    cout << "Rejected" << endl; // If none of the conditions match, print Rejected
+    getchar(); // Wait for user input
+    exit(0); // Exit the program
   }
 }
